00589b51d6908eb4f6749e566819a7bfc2187289
Danah Albaaj <dana.al.baaj@gmail.com>
2020-07-07 10:08:21 -0700

    Gatsby starter default, yay :)

---
+++ gatsby-site/src/pages/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hello!</h1>
+    <h2>Welcome to my personal website :) </h2>
+    <p>This is my first attempt at ever making a website so please don't judge me </p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link> <br />
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+  </Layout>
+)
+
+export default IndexPage
