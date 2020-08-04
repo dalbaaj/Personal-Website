8997191a490aaf7faabe4151d9c0649fb40ae93c
Danah Albaaj <dana.al.baaj@gmail.com>
2020-08-04 13:44:10 -0700

    Added ribbon for accomplishments

--- gatsby-site/src/components/pageSections/accomplishments.js
+++ gatsby-site/src/components/pageSections/accomplishments.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import Accomplishments from '../../data/accomplishments.json';
 
-const Accomplishments = () => {
+const AccomplishmentSection = () => {
     return (
         <>
             <div className="container" style={{minHeight: '20vh', padding: '5%', margin: '0px'}}>
@@ -8,13 +9,16 @@ const Accomplishments = () => {
                     <div className="col-xs-12">
                         <h1 className="is-light-sky-blue-text section-heading margin-3-b">Accomplishments</h1>
                     </div>
-                    <div className="col-xs-12">
-                        <p className="is-cultured-text">I am so cool I am on the deans list many times.</p>
-                    </div>
+                    {
+                        Accomplishments.map((accomplishment) =>
+                       <p className="ribbon"> {accomplishment.title} </p>
+                        )
+
+                    }
                 </div>
             </div>
         </>
     );
 }
 
-export default Accomplishments;
+export default AccomplishmentSection;
